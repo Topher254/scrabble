ade481d417af0cf77e584d4ec39fc9002c295b3f
Topher254 <raphaelsarota@gmail.com>
2024-06-13 15:53:24 +0300

    comment section

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
-import { FaMessage } from "react-icons/fa6";
 
 const Comments = () => {
+  const [data, setData]= useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  console.log(data)
+
   return (
     <div>
       <div className="shadow-md shadow-slate-300 p-[1em]">
         <h1 className="font-semibold p-2 text-green-600">Let's Chat</h1>
         <div className="pl-2">
-          <p>Comment1</p>
-          <p>Comment2</p>
-          <p>Comment3</p>
+        
+          
+        {data.map((d)=>(
+          <div key={d.id}>
+          <h5 className="font-semibold ">{d.username}:<span className="italic text-green-700">{d.comment}</span></h5>
+          
+          </div>
+        ))}
         </div>
         <div className="my-2 flex justify-center items-center">
-          <label  className="mr-2 font-semibold text-sm">
-           Comment:
+          <label className="mr-2 font-semibold text-sm">
+            Comment:
           </label>
           <input
             type="text"
-            
             name="username"
-            className="w-full bg-slate-200  p-1 outline-none text-green-700 "
+            className="w-full bg-slate-200 p-1 outline-none text-green-700"
+            
           />
           <a>
-          <FaPaperPlane size={20} className="ml-2 text-green-600 hover:cursor-pointer"/>
+            <FaPaperPlane size={20} className="ml-2 text-green-600 hover:cursor-pointer"/>
           </a>
-          </div>
+        </div>
       </div>
     </div>
   );
